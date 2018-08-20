d4479cd927b6c022d67c8de6cbfc52c1895f6abb
Hugh Wang <18501667737@163.com>
2018-08-20 18:41:02 +0800

    init

---
+++ src/main/java/com/uwjx/interview/JavaJobInterviewTopicApplication.java
@@ -0,0 +1,12 @@
+package com.uwjx.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJobInterviewTopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJobInterviewTopicApplication.class, args);
+    }
+}
