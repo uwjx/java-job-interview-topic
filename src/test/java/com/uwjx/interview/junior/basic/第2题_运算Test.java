ac5006b9a656b1de5e24d521704117513561a4a9
Hugh Wang <18501667737@163.com>
2018-08-21 15:04:33 +0800

    push on branch master

--- src/test/java/com/uwjx/interview/junior/basic/第2题_运算Test.java
+++ src/test/java/com/uwjx/interview/junior/basic/第2题_运算Test.java
@@ -1,5 +1,6 @@
 package com.uwjx.interview.junior.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +9,7 @@ import static org.junit.Assert.*;
  * @author : Hugh https://hughwang.me
  * @date : 2018/8/20 18:54
  */
+@Slf4j
 public class 第2题_运算Test {
 
     @Test
@@ -16,7 +18,7 @@ public class 第2题_运算Test {
         int a = 8;
         int b = a--;
         int c = 0;
-        if(a > b & ++c > 0){
+        if(a == b & ++c > 0){
             a--;
             b--;
         }
@@ -24,5 +26,12 @@ public class 第2题_运算Test {
         System.out.println("b = " + b);
         System.out.println("c = " + c);
 
+    }
+
+    @Test
+    public void t2(){
+        int a  = 8;
+        int b = a--;
+        log.warn("a = {} , b = {}" , a , b);
     }
 }
